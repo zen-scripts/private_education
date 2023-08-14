03ef26da7663831ce2360a7c7f7b7b9acfec0d0b
Ruban Tolkin <mamab@yaho.com>
2023-08-14 10:32:43 +0300

    Sendig files via terminal. Happy brutforcing =)

--- lesson_2/app.py
+++ lesson_2/app.py
@@ -1,35 +1,63 @@
 import asyncio
-
 from web3 import Web3
-
 from sdk.data.models import Networks
 from sdk.client import Client
+from loguru import logger
+from private_data import private_key1, proxy
+from data.config import FILES_DIR
 
-from private_data import private_key1, private_key2, private_key3, proxy
+
+async def check_balance(semaphore: asyncio.Semaphore):
+    async with semaphore:
+        client = Client(private_key=None, network=Networks.Ethereum, proxy=proxy)
+        # print(await client.wallet.balance(token_address='0xaf88d065e77c8cc2239327c5edb3a432268e5831'))
+        balance = None
+        retries = 1
+
+        while not balance and retries <= 5:
+            try:
+                logger.info(f"Проверяю {client.account.address}. Попытка {retries}.")
+                balance = await client.wallet.balance()
+                if balance.Wei:
+                    logger.success(f"{client.account.address}: {round(balance.Ether, 6)} ETH")
+                    await asyncio.sleep(0.2)
+                    return client.account.key.hex()
+                logger.warning(f"{client.account.address}: денег нет, но вы держитесь.")
+                await asyncio.sleep(0.2)
+                return None
+            except Exception as e:
+                logger.error(f"Ошибка при проверке баланса: {e}. Попытка: {retries}")
+                if 'Too Many Requests' in str(e):
+                    logger.critical(f"429, message='Too Many Requests': Прокси в бане {proxy}")
+                    return None
+                retries += 1
+
 
 
 async def main():
-    client = Client(private_key=private_key1, network=Networks.Optimism, proxy=proxy)
-    # print(await client.wallet.balance(token_address='0xaf88d065e77c8cc2239327c5edb3a432268e5831'))
-    balance = await client.wallet.balance()
-    balance = await client.wallet.balance()
-    balance = await client.wallet.balance()
+    accounts_number = int(input("Введите количество адресов для брута: "))
+    threads = int(input("Введите количество потоков: "))
+    semaphore = asyncio.Semaphore(threads)
+    tasks = []
+    for i in range(accounts_number):
+        tasks.append(asyncio.create_task(check_balance(semaphore)))
+    await asyncio.wait(tasks)
+    for task in tasks:
+        bruted_count = 0
+        if task.result() is not None:
+            bruted_count += 1
+            logger.success(task.result())
+            with open(f'{FILES_DIR}/bruted_accs.txt', 'a') as bruted:
+                bruted.writelines(task.result())
+    logger.info('\n\n==========================================================')
+    logger.info('СТАТИСТИКА о проделанной работе')
+    logger.info(f"Проверенно адресов: {len(tasks)}")
+    logger.info(f"Успешно сбручено: {bruted_count}")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
 
 
 
@@ -57,8 +85,3 @@ async def main():
     Она возвращает кортеж из двух множеств: множество выполненных задач и множество невыполненных задач.
     Если во время выполнения задачи возникает исключение, asyncio.wait() продолжает выполнение остальных задач и не выбрасывает исключение.
     '''
-
-
-if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(main())
